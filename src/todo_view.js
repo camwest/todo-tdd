c481868bbff1138ca1b4b1e045e851f0a9cd53cb
Cameron Westland <camwest@gmail.com>
2012-02-11 17:14:37 -0500

    Mocked model is now bound to template'

--- src/todo_view.js
+++ src/todo_view.js
@@ -1,3 +1,28 @@
 TodoView = Backbone.View.extend({
+  tagName: 'li',
 
+  initialize: function() {
+    this.on('stateChange', this.render);
+  },
+
+  events: {
+    'dblclick': function() {
+      this.$el.addClass('editing');
+      this.edit();
+    }
+  },
+
+  edit: function() {
+    this.editing = true;
+    this.trigger('stateChange');
+  },
+
+  render: function() {
+    if (this.editing) {
+      template = _.template($('#todo-edit').html());
+      this.$el.html(template(this.model.toJSON()));
+    }
+
+    return this;
+  }
 });
--- test/todo_view_test.js
+++ test/todo_view_test.js
@@ -3,10 +3,30 @@ describe('TodoView', function() {
 
   before(function() {
     sut = new TodoView();
+    sut.model = {
+      toJSON: function() {
+        return {
+          "content": "Hello World!"
+        }
+      }
+    };
+  });
+
+  it("should be an li tag", function() {
+    expect(sut.el.tagName).to.be('LI');
   });
 
   describe("when a user double clicks a todo", function() {
+    before(function() {
+      sut.$el.trigger('dblclick');
+    });
+
     it("should go into editing mode", function() {
+      expect(sut.$el.hasClass('editing')).to.be(true);
+    });
+
+    it("should have the contents of the model in the input", function() {
+      expect(sut.$el.find('.todo-input').val()).to.be("Hello World!")
     });
   });
 });
